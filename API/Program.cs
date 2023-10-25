c51b3670ed347ebfc112738515d316a3c83a3101
Le Hoang Dung <mikedung2k3@gmail.com>
2023-10-25 17:09:39 +0700

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<DataContext>(options => {
   var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
   options.UseSqlite(connectionString); 
 }); 
+builder.Services.AddCors(); 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,6 +25,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(policy => 
+  policy.AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("http://localhost:4200")
+); 
 
 app.UseAuthorization();
 
